1e8736339d8c43610f8cae79805358c01b9d16da
khose-shubham <shubhamkhose7410@gmail.com>
2025-05-19 11:36:15 +0530

    Add files via upload

---
+++ shubham FDSL 6.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Input array size
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Insertion Sort algorithm
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];  
+        int j = i - 1;
+
+        // Shift elements of arr[0..i-1] that are greater than key
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;  
+    }
+
+    // Print sorted array
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
